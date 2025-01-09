6cc81dfa341724f0cb5fae304a6a488f48dbe0d6
Marek Gogoľ <info@marekgogol.sk>
2025-01-09 22:11:14 +0100

    ref

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-  ],
-  components: {
-    dirs: [
-      {
-        path: '~/components',
-        pathPrefix: false,
-      },
-    ],
-  },
-  imports: {
-    dirs: ['stores'],
-  },
-})
+    ssr: false,
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: ['@pinia/nuxt'],
+    components: {
+        dirs: [
+            {
+                path: '~/components',
+                pathPrefix: false,
+            },
+        ],
+    },
+    imports: {
+        dirs: ['stores'],
+    },
+});
