20bcaa00c4497aca93c030ffc42c3310cc25480d
Marek Gogoľ <9mike66@gmail.com>
2025-06-29 18:12:21 +0200

    Updated attendance.vue

--- pages/attendance.vue
+++ pages/attendance.vue
@@ -158,7 +158,7 @@ const resetForm = () => {
 .page-header {
     text-align: center;
     margin-bottom: 3rem;
-    padding: 2rem 0 0;
+    padding: 1rem 0;
     border-bottom: 1px solid rgba(168, 144, 132, 0.2);
 
     @media (min-width: 768px) {
